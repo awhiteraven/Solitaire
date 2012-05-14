cc836308c42a0b646e92b5ebcaa51368ff8e97ac
Jon Wei <jonathanwei92@gmail.com>
2012-05-14 12:15:39 -0700

    Test 4

--- Solitaire/HelloWorldLayer.m
+++ Solitaire/HelloWorldLayer.m
@@ -36,7 +36,7 @@
 	if( (self=[super init])) {
 		
 		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"GIT Test 3" fontName:@"Marker Felt" fontSize:64];
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"GIT Test 4" fontName:@"Marker Felt" fontSize:64];
 
 		// ask director the the window size
 		CGSize size = [[CCDirector sharedDirector] winSize];
